b677e3c44d7a996b681387bfa066cce0e1484b5f
Wyatt <s179053@students.aie.edu.ai>
2017-12-13 11:53:02 -0800

    binary tree

--- 2-Containers/BinaryTree.h
+++ 2-Containers/BinaryTree.h
@@ -1,1 +1,23 @@
 #pragma once
+struct node
+{
+	int keyVal;
+	node *left;
+	node *right;
+};
+
+class tree
+{
+public:
+	tree();
+	~tree();
+
+	void insert(int key);
+	node *search(int key);
+	void destroy_tree();
+private:
+	void destroy_tree(node *leaf);
+	void insert(int key, node*leaf);
+	node *search(int key, node*leaf);
+	node *root;
+};
